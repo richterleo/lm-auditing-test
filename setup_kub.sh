964eeebf5c2380c2be1091123343d70a4c8f1496
richterleo <richter.leo94@gmail.com>
2024-05-02 16:36:27 +0000

    get rid of tokens and keys

--- setup_kub.sh
+++ setup_kub.sh
@@ -4,9 +4,9 @@
 # brew install md5sha1sum
 
 # Add wandb api key 
-export WANDB_API_KEY=1c84a4abed1d390fbe37478c7cb82a84e4650881 # Add your key here
 export WANDB_LOG_LEVEL=debug
-export HF_TOKEN=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
+export WANDB_API_KEY= # Add your key here
+export HF_TOKEN= # Add your key here
 
 # install flash attention
 pip install torch transformers datasets wandb numpy evaluate tqdm
