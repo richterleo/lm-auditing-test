a5c612048b63f23a3be631e9943c1ff219556503
richterleo <richter.leo94@gmail.com>
2024-05-03 13:53:44 +0000

    logging metadata to wandb

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,9 +1,7 @@
 import json
-import os
 import numpy as np
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers import pipeline, PreTrainedModel, PreTrainedTokenizer
+from transformers import pipeline, AutoTokenizer
 from transformers.utils import is_flash_attn_2_available
 
 from datasets import load_dataset
@@ -19,23 +17,41 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 args = EvalArgs(device=device)
 
 wandb.init(
-    project=f"{args.metric}_evaluation", entity="richter-leo94", name=args.run_name
+    project=f"{args.metric}_evaluation",
+    entity="richter-leo94",
+    name=args.run_name,
+    config={
+        "dataset_name": args.dataset_name,
+        "model_id": args.model_id,
+        "metric": args.metric,
+        "num_epochs": args.epochs,
+        "model_kwargs": {
+            "torch_dtype": torch.bfloat16,
+            "load_in_4bit": False,
+            # Add flash_attention_2 if available
+            **(
+                {"attn_implementation": "flash_attention_2"}
+                if is_flash_attn_2_available()
+                else {}
+            ),
+        },
+        "generation_kwargs": {
+            "max_length": 100,
+            "do_sample": True,
+            "temperature": 2.0,
+            "pad_token_id": 50256,
+            "batch_size": 8,
+        },
+    },
 )
 
+
 prompts = load_dataset(args.dataset_name, split="train")
-
-model_kwargs = {
-    "torch_dtype": torch.bfloat16,
-    "load_in_4bit": False,
-}
-
-if is_flash_attn_2_available():
-    model_kwargs.update({"attn_implementation": "flash_attention_2"})
 
 pipeline = pipeline(
     "text-generation",
     model=args.model_id,
-    model_kwargs=model_kwargs,
+    model_kwargs=wandb.config.model_kwargs,
     device=args.device,
 )
 
@@ -44,39 +60,35 @@ tokenizer = AutoTokenizer.from_pretrained(args.model_id)
 sample = get_random_prompts(prompts)
 prompts = [p["text"] for p in sample["prompt"]]
 
-scores = {}
+results = {}
 
 metric = evaluate.load(args.metric)  # Aggregate different metrics here
+
 
 for epoch in tqdm(range(args.epochs)):
     model_continuations = []
     for prompt in tqdm(prompts):
-        generation = pipeline(
-            prompt,
-            max_length=100,
-            do_sample=True,
-            temperature=2.0,
-            pad_token_id=50256,
-            batch_size=8,
-        )
+        generation = pipeline(prompt, generation_kwargs=wandb.config.generation_kwargs)
 
         continuation = generation[0]["generated_text"].replace(prompt, "")
         model_continuations.append(continuation)
 
     ratings = metric.compute(predictions=model_continuations)
 
-    scores[epoch] = ratings[args.metric]
-    print(scores[epoch])
+    results[epoch]["generation"] = model_continuations
+    results[epoch]["rating"] = ratings[args.metric]
 
-    hist = np.histogram(scores[epoch], bins="auto")
-    wandb.log({f"Epoch {epoch+1} Toxicity Scores": wandb.Histogram(np_histogram=hist)})
+    hist = np.histogram(results[epoch], bins="auto")
+    wandb.log(
+        {f"Epoch {epoch+1} {args.metric} scores": wandb.Histogram(np_histogram=hist)}
+    )
 
-    log_scores(scores)
+    log_scores(results)
 
     file_name = f"{args.metric}_scores.json"
 
     with open(file_name, "w") as file:
-        json.dump(scores, file, indent=4)
+        json.dump(results, file, indent=4)
 
     print(f"Saved scores epoch {epoch} out of {args.epochs}.")
 
