068252f2b5f2823f33aa6753136aecda5087360f
richterleo <richter.leo94@gmail.com>
2024-09-26 18:30:42 +0000

    changed seeds for weird checkpoints

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,15 +2,19 @@
 
 # running all tests
 for combination in "gemma-1.1-7b-it gemma-1.1-7b-it-ckpt" "Meta-Llama-3-8B-Instruct Llama-3-8B-ckpt" "Mistral-7B-Instruct-v0.2 Mistral-7B-Instruct-ckpt"; do
-  set -- $combination
-  model_name1=$1
-  model_name2=$2
-  for fold_size in 1000 2000 3000 4000; do
-    for i in {1..10}; do
-      python -u main.py --exp "test_daht" --no_wandb --model_name1 "$model_name1" --model_name2 "${model_name2}$i" --fold_size $fold_size > test_${model_name1}_${model_name2}_${i}_${fold_size}_output.txt 2>&1 &
-    done
-    wait
-  done
+set -- $combination
+model_name1=$1
+model_name2=$2
+for fold_size in 1000 2000 3000 4000; do
+for i in {1..10}; do
+    if [[ $model_name2 == Llama-3-8B-ckpt && $i -le 4 ]]; then
+        python -u main.py --exp "test_daht" --no_wandb --model_name1 "$model_name1" --model_name2 "${model_name2}$i" --fold_size $fold_size --seed2 seed2000 > test_${model_name1}_${model_name2}_${i}_${fold_size}_output.txt 2>&1 &
+    else
+        python -u main.py --exp "test_daht" --no_wandb --model_name1 "$model_name1" --model_name2 "${model_name2}$i" --fold_size $fold_size > test_${model_name1}_${model_name2}_${i}_${fold_size}_output.txt 2>&1 &
+    fi
+done
+wait
+done
 done
 
 
